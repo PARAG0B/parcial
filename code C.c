118b3ef43fd2bb0c6606a81ce6d80c3fdabb9f5c
PARAG0B <166534074+PARAG0B@users.noreply.github.com>
2024-06-02 22:44:48 -0500

    entrega coex
    buenos, a traves de este repositorio subo tanto el código de c++ y c del juego del dinosaurio.

---
+++ code C.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+#define WIDTH 50
+#define HEIGHT 10
+#define JUMP_HEIGHT 5
+#define GAME_SPEED 50
+
+int gameOver;
+int score;
+int dinoX, dinoY;
+int cactusX, cactusY;
+
+void setup() {
+    gameOver = 0;
+    score = 0;
+    dinoX = 5;
+    dinoY = HEIGHT - 1;
+    cactusX = WIDTH - 5;
+    cactusY = HEIGHT - 1;
+}
+
+void draw() {
+    system("cls");
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == dinoY && j == dinoX)
+                printf("R");
+            else if (i == cactusY && j == cactusX)
+                printf("+");
+            else if (i == HEIGHT - 1)
+                printf("_");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("Score: %d\n", score);
+}
+
+void jump() {
+    if (dinoY >= HEIGHT - JUMP_HEIGHT)
+        dinoY -= JUMP_HEIGHT;
+}
+
+void input() {
+    if (_kbhit()) {
+        char key = _getch();
+        if (key == ' ')
+            jump();
+    }
+}
+
+void update() {
+    if (dinoY < HEIGHT - 1)
+        dinoY++;
+    cactusX--;
+    if (cactusX <= 0) {
+        cactusX = WIDTH - 5;
+        score++;
+    }
+    if ((cactusX == dinoX) && (cactusY == dinoY))
+        gameOver = 1;
+}
+
+int main() {
+    setup();
+    while (!gameOver) {
+        draw();
+        input();
+        update();
+        Sleep(GAME_SPEED);
+    }
+    printf("\nGame Over! Final Score: %d\n", score);
+    return 0;
+}
